b7a66d86abb69f70c9b11dbecfbc4d17b083a2f4
kamiliiaa <nadayasin106@yahoo.co.id>
2021-02-24 15:32:46 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,23 @@
+<?php
+
+    define("PHI",3.14);
+    $jari_jari = 7;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo $luas_lingkaran;
+
+    $angka = 1;
+    $nama = "Nada";
+    $nama_lengkap = "Nada Kamilia";
+    $angka_desimal = 3.5;
+
+    echo $angka;
+    echo "</br>";
+    echo $nama;
+    echo "</br>";
+    echo $angka_desimal;
+
+    echo "</br>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+    echo " nama saya ".$nama_lengkap
+    
+  ?>
